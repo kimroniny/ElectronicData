c269e42fe9c1de4bc286fe2d4910358aae480be2
kimroniny <1301862177@qq.com>
2019-11-07 11:29:59 +0800

    update capture.py specify path of arial.ttf

--- app/capture.py
+++ app/capture.py
@@ -1,7 +1,8 @@
 import random
 import string
 from PIL import Image, ImageFont, ImageDraw, ImageFilter
-from flask import url_for
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 def rndColor():
@@ -29,7 +30,7 @@ def get_verify_code():
     # 新图片对象
     im = Image.new('RGB',(width, height),'white')
     # 字体
-    font = ImageFont.truetype(url_for('static', filename='arial.ttf'), 40)
+    font = ImageFont.truetype(os.path.join(basedir, 'static', 'font', 'arial.ttf'), 40)
     # draw对象
     draw = ImageDraw.Draw(im)
     # 绘制字符串
